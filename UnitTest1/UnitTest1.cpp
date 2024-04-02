1d3c9f87569f1c3264383a116824f00e1527a1ca
Hardwik1337 <karta007pryadko@gmail.com>
2024-04-02 18:31:43 +0300

    Add project files.

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "set.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace SetTests
+{
+    TEST_CLASS(SetTest)
+    {
+    public:
+
+        TEST_METHOD(TestAdd)
+        {
+          
+        }
+
+      
+    };
+}
